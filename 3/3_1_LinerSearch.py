d9bcf5f54a7ac846ee5d81431e1e288acec63a1b
goto_ryu <viefabri@gmail.com>
2025-06-14 15:38:08 +0900

    3_1添字を出力するように変更

--- 3/3_1_LinerSearch.py
+++ 3/3_1_LinerSearch.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+_noData = -1
+
 def linear_search_v1():
+    
+    ans = _noData
+    
     """
     C++のコードのロジックを忠実に再現したバージョン
     """
@@ -27,44 +32,46 @@ def linear_search_v1():
     exist = False  # 初期値は False に
     for i in range(N):
         if a[i] == v:
-            exist = True  # 見つかったらフラグを立てる
+            #exist = True  # 見つかったらフラグを立てる
+            ans = i
             break # 見つかった時点でループを抜けるのが効率的
 
     # 結果出力
-    if exist:
-        print("Yes")
-    else:
-        print("No")
+    #if exist:
+    #    print("Yes")
+    #else:
+    #    print("No")
+    print(ans)
 
 
-def linear_search_v2():
-    """
-    よりPythonらしい、シンプルな書き方のバージョン
-    """
-    # Nとvの入力を受け取る (Nは実際には使わないが、入力形式を合わせるため)
-    try:
-        _, v_str = input("配列の要素数 N と探す値 v をスペース区切りで入力してください: ").split()
-        v = int(v_str)
-    except ValueError:
-        print("正しい数値を入力してください。")
-        return
+# def linear_search_v2():
+#     """
+#     よりPythonらしい、シンプルな書き方のバージョン
+#     """
+#     # Nとvの入力を受け取る (Nは実際には使わないが、入力形式を合わせるため)
+#     try:
+#         _, v_str = input("配列の要素数 N と探す値 v をスペース区切りで入力してください: ").split()
+#         v = int(v_str)
+#     except ValueError:
+#         print("正しい数値を入力してください。")
+#         return
 
-    # 配列aの入力を受け取る
-    try:
-        a = list(map(int, input("整数をスペース区切りで入力してください: ").split()))
-    except ValueError:
-        print("正しい数値を入力してください。")
-        return
+#     # 配列aの入力を受け取る
+#     try:
+#         a = list(map(int, input("整数をスペース区切りで入力してください: ").split()))
+#     except ValueError:
+#         print("正しい数値を入力してください。")
+#         return
 
-    # Pythonの `in` 演算子を使って探索
-    if v in a:
-        print("Yes")
-    else:
-        print("No")
+#     # Pythonの `in` 演算子を使って探索
+#     if v in a:
+#         print("Yes")
+#     else:
+#         print("No")
 
 
 if __name__ == '__main__':
     print("--- C++のロジックを再現したバージョン ---")
     linear_search_v1()
-    print("\n--- Pythonらしいシンプルなバージョン ---")
-    linear_search_v2()
+    # print("\n--- Pythonらしいシンプルなバージョン ---")
+    # linear_search_v2()
